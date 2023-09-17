6a6c141e1d936feedfd443bcceb17c18fc3afd1b
MoustafaDarwish <144167995+Moustafadarwish98@users.noreply.github.com>
2023-09-17 07:26:32 +0300

    Delete Quiz_Game directory

--- Quiz_Game/quiz_model.py
+++
@@ -1,8 +0,0 @@
-class Questions:
-    """Specify the question text, correct answer and all choices."""
-    def __init__(self, question_text, answers, correct_answer):
-        self.question_text = question_text
-        self.correct_answer = correct_answer
-        self.all_choices = answers
-
-
